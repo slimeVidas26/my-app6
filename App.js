dd515c1945dec5abe9f012c554fc8615a43a9338
SlimeVidas <dahan.isaac@gmail.com>
2023-09-07 19:07:34 +0300

    homeScreen

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import React  , {useEffect , useState , useLayoutEffect} from "react";
 import OrderInfoState from "./screens/Foods/Grid/arrival/EntryCertificateEDI/store/OrderInfoState";
 import EDIContext from "./screens/Foods/Grid/arrival/EntryCertificateEDI/store/EDIContext";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View , Button ,Image,
   TextInput,TouchableOpacity, ScrollView } from 'react-native';
   import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -23,7 +23,7 @@ import { StyleSheet, Text, View , Button ,Image,
 
 
 
-
+import { HomeScreen } from './screens/HomeScreen';
 import {GridScreen  } from './screens/GridScreen';
 import { LoginScreen } from './LoginScreen';
 import {HelloScreen} from './HelloScreen';
@@ -44,43 +44,23 @@ import {SignFormScreen} from './screens/Foods/Grid/arrival/EntryCertificateEDI/O
 
 
 
-import { translation } from './i18n/supportedLanguages';
-import * as Localization from 'expo-localization';
-import { I18n } from 'i18n-js';
-// Set the key-value pairs for the different languages you want to support.
-const i18n = new I18n(translation);
-// Set the locale once at the beginning of your app.
-i18n.locale = Localization.locale;
-// When a value is missing from a language it'll fallback to another language with the key present.
-i18n.enableFallback = true;
+ import { translation } from './i18n/supportedLanguages';
+ import * as Localization from 'expo-localization';
+ import { I18n } from 'i18n-js';
+// // Set the key-value pairs for the different languages you want to support.
+ const i18n = new I18n(translation);
+// // Set the locale once at the beginning of your app.
+ i18n.locale = Localization.locale;
+// // When a value is missing from a language it'll fallback to another language with the key present.
+ i18n.enableFallback = true;
 // To see the fallback mechanism uncomment line below to force app to use Japanese language.
- //i18n.locale = 'he';
+ i18n.locale = 'he';
 
  //client.httpCache.clearAll()
 
  
 
 
-function HomeScreen({navigation}){
-
-//   console.log('OrderInfoContext' , OrderInfoContext)
-//  console.log('OrderInfoState' , OrderInfoState())
-  return (
-    
-    <View style={styles.container}>
-    <Text style={styles.text}>
-        {i18n.t('welcome')} {i18n.t('name')}
-      </Text>
-      <Text style={styles.text}>Current locale: {i18n.locale}</Text>
-      <Text style={styles.text}>Device locale: {Localization.locale}</Text>
-      <StatusBar style="auto" />
-      <Button
-        title="Go to Login"
-        onPress={() => navigation.navigate('Login')}
-      />
-    </View>
-  );
-}
 
 // const screenOptions = (route, color) => {
 //   let iconName;
@@ -343,41 +323,4 @@ function App() {
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 
-  inputView: {
-    backgroundColor: "#1f1400",
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
- 
-    alignItems: "center",
-  },
- 
-  TextInput: {
-    height: 50,
-    flex: 1,
-    padding: 10,
-    marginLeft: 20,
-  },
-  loginBtn: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "blue",
-  },
-  text:{
-    fontSize:20
-  }
- 
-});
