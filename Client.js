111b718532cddec38f51f48f86273345c0ba1d48
SlimeVidas <dahan.isaac@gmail.com>
2023-12-27 06:18:56 +0200

    test

--- Client.js
+++ Client.js
@@ -4,6 +4,6 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 
 export const client = new ApolloClient({
-    uri: 'http://192.168.133.1:3998/',
+    uri: 'http://192.168.133.1:4000/',
     cache: new InMemoryCache()
   });
